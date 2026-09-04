531201217160a88ded7b0d7bc2807089b82c71ec
Eric Sousa <ericsousa@gmail.com>
2026-09-03 21:25:58 -0300

    feat: implement LoggerService and ProdutoService

--- angular/loja/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ angular/loja/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,6 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { CardProduto } from '../card-produto/card-produto';
+import { ProdutoService } from '../services/produto.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -10,14 +12,18 @@ import { CardProduto } from '../card-produto/card-produto';
 })
 export class ListaProdutos {
 
+
+  private produtoService = inject(ProdutoService);
+  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar(),{ initialValue: [] }); 
+
   // flag de exibir apeas produtos em promoção
   apenasPromo = signal(false);
 
   // cria a lista de produtos a ser exibida com base na flag apenasPromo
   produtosExibidos = computed(() =>
     this.apenasPromo()
-    ? this.produtos.filter(p => p.promo)
-    : this.produtos
+    ? this.produtos().filter(p => p.promo)
+    : this.produtos()
   );
 
 
@@ -26,32 +32,7 @@ export class ListaProdutos {
     this.apenasPromo.update(v => !v); // Alterna o valor do sinal apenasPromo entre true e false
   }
   
-  produtos = <Produto[]>[
-    {
-      id: 1,
-      nome: 'Patola Mp055',
-      preco: 1001.00,
-      descricao: 'Patola Mp055 é uma patola de alta qualidade, preta.',
-      imageUrl: 'images/produto1.jpg',
-      promo: false
-    },
-    {
-      id: 2,
-      nome: 'Patola Mp055',
-      preco: 1002.00,
-      descricao: 'Patola Mp055 é uma patola de alta qualidade, azul.',
-      imageUrl: 'images/produto2.jpg',
-      promo: false
-    },
-    {
-      id: 3,
-      nome: 'Patola Mp055',
-      preco: 950.00,
-      descricao: 'Patola Mp055 é uma patola de alta qualidade, vermelha.',
-      imageUrl: 'images/produto3.jpg',
-      promo: true
-    }
-  ]
+  
 
   onViewProduct(id: number) {
     alert(`Visualizar produto com id: ${id}`);
---
+++ angular/loja/src/app/features/produtos/services/produto.service.ts
@@ -0,0 +1,49 @@
+import { inject, Injectable} from '@angular/core';
+import { LoggerService } from '../../../core/services/logger/logger.service';
+import { Produto } from '../../../model/produto';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
+import { delay } from 'rxjs/internal/operators/delay';
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class ProdutoService {
+    private logger = inject(LoggerService);
+
+    private readonly listaMock = <Produto[]>[
+    {
+      id: 1,
+      nome: 'Patola Mp055',
+      preco: 1001.00,
+      descricao: 'Patola Mp055 é uma patola de alta qualidade, preta.',
+      imageUrl: 'images/produto1.jpg',
+      promo: false
+    },
+    {
+      id: 2,
+      nome: 'Patola Mp055',
+      preco: 1002.00,
+      descricao: 'Patola Mp055 é uma patola de alta qualidade, azul.',
+      imageUrl: 'images/produto2.jpg',
+      promo: false
+    },
+    {
+      id: 3,
+      nome: 'Patola Mp055',
+      preco: 950.00,
+      descricao: 'Patola Mp055 é uma patola de alta qualidade, vermelha.',
+      imageUrl: 'images/produto3.jpg',
+      promo: true
+    }
+  ]
+
+  listar(): Observable<Produto[]> {
+    this.logger.info("[PRODUTO SERVICE] - Retornando lista de produtos")
+    return of(this.listaMock).pipe(
+        delay(250)
+    );
+  }
+}
+
